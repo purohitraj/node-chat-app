75bb11db281035dcbce4841952d9eb32c0c6e81a
raj <raj.purohit@workhiveapp.com>
2018-07-31 22:01:23 +0530

    Add connect and disconnect event

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const app = express();
-
+const socketIO = require('socket.io');
 const path = require('path');
+const http = require('http');
+
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
-// app.get('/',(req,res)=>{
-// 	res.send(publicPath+'/index.html')
-// })
+
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection',(socket)=>{
+	console.log('new user connected');
+	
+	socket.on('disconnect',()=>{
+		console.log('user disconected');
+	});
+});
+
+
 app.use(express.static(publicPath))
 
-app.listen(port,() => {
+server.listen(port,() => {
 	console.log(`Start Localhost at port ${port}`)
 })
