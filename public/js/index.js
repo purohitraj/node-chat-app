51d259a55e3383af7abeba382da4f8bd9bdcbf3e
raj <raj.purohit@workhiveapp.com>
2018-08-04 19:48:33 +0530

    multitab functionality

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,10 @@ var socket = io();
 socket.on('connect',function(){
 	console.log('connected to server');
 
-	socket.emit('createMessage',{
-		from:'Raj',
-		text:'hello'
-	})
+	// socket.emit('createMessage',{
+	// 	from:'Raj',
+	// 	text:'hello'
+	// })
 });
 
 socket.on('disconnect',function(){
--- server/server.js
+++ server/server.js
@@ -14,14 +14,19 @@ var io = socketIO(server);
 io.on('connection',(socket)=>{
 	console.log('new user connected');
 	
-	socket.emit('newMessage',{
-		from:'Raj Purohit',
-		text:'Hello Raj',
-		createdAt:123
-	});
+	// socket.emit('newMessage',{
+	// 	from:'Raj Purohit',
+	// 	text:'Hello Raj',
+	// 	createdAt:123
+	// });
 
 	socket.on('createMessage',(message)=> {
 		console.log(message);
+		io.emit('newMessage',{
+			from:message.from,
+			text:message.text,
+			createdAt:new Date().getTime()
+		})
 	})
 	socket.on('disconnect',()=>{
 		console.log('user disconected');
