9b19ea106b2db4b774ca398ae900ca7600d78d2d
raj <raj.purohit@workhiveapp.com>
2018-08-04 18:50:19 +0530

    addMessage and createMessage event

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+socket.on('connect',function(){
+	console.log('connected to server');
+
+	socket.emit('createMessage',{
+		from:'Raj',
+		text:'hello'
+	})
+});
+
+socket.on('disconnect',function(){
+	console.log('disconnected to server');
+});
+
+socket.on('newMessage',function(message){
+	console.log('Add New Message');
+	console.log(message);
+});
--- server/server.js
+++ server/server.js
@@ -14,6 +14,15 @@ var io = socketIO(server);
 io.on('connection',(socket)=>{
 	console.log('new user connected');
 	
+	socket.emit('newMessage',{
+		from:'Raj Purohit',
+		text:'Hello Raj',
+		createdAt:123
+	});
+
+	socket.on('createMessage',(message)=> {
+		console.log(message);
+	})
 	socket.on('disconnect',()=>{
 		console.log('user disconected');
 	});
