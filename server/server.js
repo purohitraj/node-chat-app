55a2a9c198537715d1e2e463e1b61019a386ae62
raj <raj.purohit@workhiveapp.com>
2018-08-05 00:33:43 +0530

    create generateMessage utillity

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const socketIO = require('socket.io');
 const path = require('path');
 const http = require('http');
 
+const {generateMessage} = require('./utils/message');
+
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
@@ -14,25 +16,13 @@ var io = socketIO(server);
 io.on('connection',(socket)=>{
 	console.log('new user connected');
 	
-	socket.emit('newMessage',{
-		from:'Admin',
-		text:'Welcome to chat app',
-		createdAt:new Date().getTime()
-	});
+	socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
 
-	socket.broadcast.emit('newMessage',{
-		from:'Admin',
-		text:'New User Connected',
-		createdAt:new Date().getTime()
-	});
+	socket.broadcast.emit('newMessage',generateMessage('Admin','New User Connected'));
 
 	socket.on('createMessage',(message)=> {
 		console.log(message);
-		io.emit('newMessage',{
-			from:message.from,
-			text:message.text,
-			createdAt:new Date().getTime()
-		})
+		io.emit('newMessage',generateMessage(message.from,message.text))
 
 		// socket.broadcast.emit('newMessage',{
 		// 	from:message.from,
