2f91f6c59632aa27cfdd93db2720757b4f79ccef
raj <raj.purohit@workhiveapp.com>
2018-08-04 21:00:25 +0530

    welcome emit and broadcast

--- server/server.js
+++ server/server.js
@@ -14,11 +14,17 @@ var io = socketIO(server);
 io.on('connection',(socket)=>{
 	console.log('new user connected');
 	
-	// socket.emit('newMessage',{
-	// 	from:'Raj Purohit',
-	// 	text:'Hello Raj',
-	// 	createdAt:123
-	// });
+	socket.emit('newMessage',{
+		from:'Admin',
+		text:'Welcome to chat app',
+		createdAt:new Date().getTime()
+	});
+
+	socket.broadcast.emit('newMessage',{
+		from:'Admin',
+		text:'New User Connected',
+		createdAt:new Date().getTime()
+	});
 
 	socket.on('createMessage',(message)=> {
 		console.log(message);
@@ -27,6 +33,12 @@ io.on('connection',(socket)=>{
 			text:message.text,
 			createdAt:new Date().getTime()
 		})
+
+		// socket.broadcast.emit('newMessage',{
+		// 	from:message.from,
+		// 	text:message.text,
+		// 	createdAt:new Date().getTime()
+		// })
 	})
 	socket.on('disconnect',()=>{
 		console.log('user disconected');
