ef4be71fdbb9db3bc26a684279444a01fe9bf530
raj <raj.purohit@workhiveapp.com>
2018-08-05 12:51:03 +0530

    add form

--- public/js/index.js
+++ public/js/index.js
@@ -15,4 +15,27 @@ socket.on('disconnect',function(){
 socket.on('newMessage',function(message){
 	console.log('Add New Message');
 	console.log(message);
+
+	var li = jQuery('<li></li>');
+	li.text(`${message.from}:${message.text}`);
+	jQuery('#messages').append(li);
 });
+
+// socket.emit('createMessage',{
+// 	from:'Raj',
+// 	text:'Hiii'
+// },function(data) {
+// 	console.log(data);
+// })
+
+$(document).ready(function() {
+	$('#message-form').on('submit',function(e) {
+		e.preventDefault();
+
+		socket.emit('createMessage',{
+			from:'User',
+			text:$('[name=message]').val()
+		},function(){
+		});
+	})
+})
